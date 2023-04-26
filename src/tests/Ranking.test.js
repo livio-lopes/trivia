59639a382f43852165971ec489113a63b8a538b7
lacerda-akm <lucas.lacerda@akm.com.br>
2023-04-26 17:52:25 -0300

    req 20 feito

--- src/tests/Ranking.test.js
+++ src/tests/Ranking.test.js
@@ -21,6 +21,7 @@ describe('Testa se o botão do ranking vai para a home', () => {
       assertions: 1
     }
   }
+
 const LocalStorage = [
   {
       "imgUrl": "https://www.gravatar.com/avatar/434a9c2c07210ce4d1176457876f37b2",
@@ -28,6 +29,7 @@ const LocalStorage = [
       "score": 5,
   },
 ]
+
   it('Testa se o botão redireciona para a pagina home', () => {
     localStorage.setItem('ranking', JSON.stringify(LocalStorage))
     const { history } = renderWithRouterAndRedux(<App />, initialState, '/ranking')
